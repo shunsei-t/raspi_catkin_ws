8a9c248ab34534fb9b788cc7d449fad23c718544
shunsei-t <shunsei3t6@gmail.com>
2022-02-14 16:23:18 +0900

    modify encorder

--- src/ros_lecture/hard_lecture/platformio/uno/src/main.ino
+++ src/ros_lecture/hard_lecture/platformio/uno/src/main.ino
@@ -1,46 +1,71 @@
 #include <ros.h>
-#include <std_msgs/Int16.h>
-#define MA_PULSE_A 5
-#define MA_PULSE_B 3
-#define MB_PULSE_A
+#include <std_msgs/Int32MultiArray.h>
+#define PULSE_MA_A 5
+#define PULSE_MA_B 3
+#define PULSE_MB_A 4
+#define PULSE_MB_B 2
 
 ros::NodeHandle  nh;
-std_msgs::Int16 int_msg;
-ros::Publisher row_count("row_count", &int_msg);
-int count = 0;
+std_msgs::Int32MultiArray array_msg;
+ros::Publisher row_count("row_count_MA", &array_msg);
+
+int count[2] = 0;
 
 void setup()
 {
   nh.initNode();
   nh.advertise(row_count);
 
-  pinMode(PULSE_A, INPUT);
-  pinMode(PULSE_B, INPUT);
+  pinMode(PULSE_MA_A, INPUT);
+  pinMode(PULSE_MA_B, INPUT);
+  pinMode(PULSE_MB_A, INPUT);
+  pinMode(PULSE_MB_B, INPUT);
 
-  attachInterrupt(0, callbask, CHANGE);
+  attachInterrupt(1, callback_MA, CHANGE);
+  attachInterrupt(0, callback_MB, CHANGE);
 }
 
 void loop()
 {
-  int_msg.data = count;
+  array_msg.data[0] = count[0];
+  array_msg.data[1] = count[1];
   nh.spinOnce();
 }
 
-void callbask(void) {  
-  if     ((digitalRead(PULSE_A) == true) && (digitalRead(PULSE_B) == true)) {
-    count++;
-    row_count.publish( &int_msg );
+void callback_MA(void) {  
+  if     ((digitalRead(PULSE_MA_A) == true) && (digitalRead(PULSE_MA_B) == true)) {
+    count[0]++;
+    row_count.publish( &array_msg );
   }
-  else if((digitalRead(PULSE_A) == true) && (digitalRead(PULSE_B) == false)) {
-    count--;
-    row_count.publish( &int_msg );
+  else if((digitalRead(PULSE_MA_A) == true) && (digitalRead(PULSE_MA_B) == false)) {
+    count[0]--;
+    row_count.publish( &array_msg );
   }
-  else if((digitalRead(PULSE_A) == false) && (digitalRead(PULSE_B) == true)) {
-    count--;
-    row_count.publish( &int_msg );
+  else if((digitalRead(PULSE_MA_A) == false) && (digitalRead(PULSE_MA_B) == true)) {
+    count[0]--;
+    row_count.publish( &array_msg );
   }
-  else if((digitalRead(PULSE_A) == false) && (digitalRead(PULSE_B) == false)) {
-    count++;
-    row_count.publish( &int_msg );
+  else if((digitalRead(PULSE_MA_A) == false) && (digitalRead(PULSE_MA_B) == false)) {
+    count[0]++;
+    row_count.publish( &array_msg );
+  }
+}
+
+void callback_MB(void) {  
+  if     ((digitalRead(PULSE_MB_A) == true) && (digitalRead(PULSE_MB_B) == true)) {
+    count[1]++;
+    row_count.publish( &array_msg );
+  }
+  else if((digitalRead(PULSE_MB_A) == true) && (digitalRead(PULSE_MB_B) == false)) {
+    count[1]--;
+    row_count.publish( &array_msg );
+  }
+  else if((digitalRead(PULSE_MB_A) == false) && (digitalRead(PULSE_MB_B) == true)) {
+    count[1]--;
+    row_count.publish( &array_msg );
+  }
+  else if((digitalRead(PULSE_MB_A) == false) && (digitalRead(PULSE_MB_B) == false)) {
+    count[1]++;
+    row_count.publish( &array_msg );
   }
 }
